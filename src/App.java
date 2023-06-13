27c07543e250b710b18e02c9776b7e17e209c04f
Alejandro Garduza <alexgm412@gmail.com>
2023-06-12 22:26:25 -0600

    Revert Feature: Solo imprime los impares"
    This reverts commit adad8500fec557ab718d481ad3119a3fbdc1611f.

--- src/App.java
+++ src/App.java
@@ -20,11 +20,11 @@ public class App {
         
         if (numUno<numDos) {
             for(int i=(numUno+1);i<numDos;i++){
-                if(i%2==0) rango.add(i);
+                rango.add(i);
             }            
         }else{
             for(int i = (numDos+1); i<numUno; i++){
-                if(i%2==0) rango.add(i);
+                rango.add(i);
             }
         }
         
